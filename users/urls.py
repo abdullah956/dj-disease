0f701c9d94b8b5a7fb28eec6ebe4f519deaad9be
abdullah956 <abdullaharshed956@gmail.com>
2024-09-13 00:09:44 +0500

    auth and oocr done

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index_view
+from .views import index_view ,  UserRegisterView, UserLoginView , logout_view
 
 urlpatterns = [
-    path('', index_view, name='index_view'),
+    path('', index_view, name='home'),
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.views import View
+from .forms import UserRegistrationForm, UserLoginForm
+from django.contrib.auth import logout
 
 def index_view(request):
     return render(request, 'index.html')
+
+
+class UserRegisterView(View):
+    def get(self, request):
+        form = UserRegistrationForm()
+        return render(request, 'users/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        return render(request, 'users/register.html', {'form': form})
+
+
+class UserLoginView(View):
+    def get(self, request):
+        form = UserLoginForm()
+        return render(request, 'users/login.html', {'form': form})
+
+    def post(self, request):
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('home') 
+        return render(request, 'users/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
